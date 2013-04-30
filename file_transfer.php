1d5308a8c45e2ee79ace0a840a84ebee24a91657
Ross Miller <rgmiller@ornl.gov>
2013-04-30 10:56:04 -0400

    Use multi-part MIME instead of query strings to pass data in HTTP POST (for file uploads)

--- file_transfer.php
+++ file_transfer.php
@@ -11,6 +11,10 @@
  * Query strings for file queries will need 2 variables (and note that Action is different):
  * TransID=xxxx&Action=Query
  * 
+ * HTTP POST's don't have query strings, so the 'Action' and 'TransID' variables must
+ * be transmitted in multipart mime format.  Note that the file names (multiple files
+ * can be uploaded) are passed in the header for the particular part.
+ * 
  * 
  */
  
@@ -47,33 +51,79 @@
     // an exception if there's an error, so if it returns,
     // we know we're good to go.
     ldap_auth();
-    
-    // The file/directory we're interested in is looked up using the transaction
-    // ID, which is specified as a GET var or POST var  
-    $_GET_lower = array_change_key_case($_GET, CASE_LOWER);
-    $_POST_lower = array_change_key_case( $_POST, CASE_LOWER);
+
+	try {
+        
+    // Validate the query string
     $transId = "";  // empty strings are equivalent to boolean false
+    $action = "";
+    $file = "";
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    	$_GET_lower = array_change_key_case($_GET, CASE_LOWER);
+    	
 		if ( array_key_exists( 'transid', $_GET_lower)) {
     		$transId = $_GET_lower['transid'];
 		}
+		
+		if ( array_key_exists( 'action', $_GET_lower)) {
+			$action = strtolower( $_GET_lower['action']);
+		}
+		
+		if ( array_key_exists( 'file', $_GET_lower)) {
+			$file = $_GET_lower['file'];
+		}
     }
     elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    	$_POST_lower = array_change_key_case( $_POST, CASE_LOWER);
+    	
 		if ( array_key_exists( 'transid', $_POST_lower)) {
     		$transId = $_POST_lower['transid'];
 		}
+		if ( array_key_exists( 'action', $_POST_lower)) {
+			$action = strtolower( $_POST_lower['action']);
+		}
     }
+    
     if ( ! $transId) {
-        // Transaction ID must be specified on the command line
+        // Transaction ID must be specified in the query string
         header( 'HTTP/1.1 400 Bad Request');
         echo "TransID parameter not specified.";
         return;
     }
+    
+    if ( ! $action) {
+        // Action must be specified in the query string
+        header( 'HTTP/1.1 400 Bad Request');
+        echo "Action parameter not specified.";
+        return;
+    }
 
-    try {
-    $pdo = open_db();
+    // Only download and queries can use GET requests
+    if ($_SERVER['REQUEST_METHOD'] == 'GET' &&
+        $action != "download" &&
+        $action != "query" ) {
+		header( 'HTTP/1.1 400 Bad Request');
+		echo "Unrecognized 'action' type for a GET request";
+		return;
+	}
+    
+    // Only uploads can use POST requests
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' &&
+        $action != "upload") {
+		header( 'HTTP/1.1 400 Bad Request');
+		echo "Unrecognized 'action' type for a POST request";
+		return;
+    }
+    
+    // If it's a download request, there must be a file variable
+    if ($action == "download" && ! $file) {
+		header( 'HTTP/1.1 400 Bad Request');
+		echo "File to download not specified.";
+		return;	
+    }
 
     // Check to see if the user is asking about one of his own transactions
+    $pdo = open_db();
     $user = find_user( $pdo, $transId);
     if ($user === false) {
         header( 'HTTP/1.1 404 Not Found');
@@ -88,96 +138,71 @@
         // it confirms the existance of a transaction that the user doesn't own.
         return;
     }
-    
-	// uploads are posts, downloads & queries are gets
-	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-		// Check the 'action' GET var: should be either 'download' or query
-		if (! array_key_exists( 'action', $_GET_lower)) {
-			header( 'HTTP/1.1 400 Bad Request');
-			echo "No action specified.";
-			return;
-		}
-		
-		$action = strtolower( $_GET_lower['action']);
-		
-		// Handle file download requests
-		if ($action == "download") {
-			// Check the 'file' GET var
-			$file = $_GET_lower['file'];
-			if (! $file ) {
-				header( 'HTTP/1.1 400 Bad Request');
-        		echo "File to download not specified.";
-        		return;	
-			}
-			
-			$fullPathName = find_directory( $pdo, $transId) . DIRECTORY_SEPARATOR . $file;	
-			if ( ! file_exists( $fullPathName)) {
-	        	// file not found
-	        	header( 'HTTP/1.1 404 Not Found');
-            	echo "The specified file - $file - could not be found.";
-            	return;
-        	}
-	
-		    if (filesize( $fullPathName) === false) {
-		        // File exists, but we can't read it.  Probably a permissions or
-		        // selinux issue.
-		        header( 'HTTP/1.1 500 Internal Server Error');
-		        echo "Cannot read $file.";
-		        return;
-		    }
-			
-			// Actually transfer the file
-	        header( 'HTTP/1.1 200 OK');
-	        header('Content-Description: File Transfer');
-	        header('Content-Type: application/octet-stream');
-	        header('Content-Disposition: attachment; filename=' . $file);
-	        header('Content-Transfer-Encoding: binary');
-	        header('Expires: 0');
-	        header('Cache-Control: must-revalidate');
-	        header('Pragma: public');
-	        header('Content-Length: ' . filesize($fullPathName));
-	        ob_clean();
-	        flush();
-	        readfile($fullPathName);
-			return;
-			
-		} elseif ($action == "query") {
-			// Get a directory listing and return it as a JSON object
-			$dir = find_directory( $pdo, $transId);
-			
-			$listing = array();
-			if ($handle = opendir($dir)) {
-			    // loop over the directory
-			    while (false !== ($entry = readdir($handle))) {
-			    	if ($entry != '.' && $entry != '..') {
-			    		$listing[] = $entry;
-			    		// appenda entry to the listing (but doesn't include
-			    		// . or .. directories)
-			    	}
-			    }
-
-			    closedir($handle);
-			    
-			    // If we just $listing now, we'd only get an array, not a complete
-			    // JSON object (ie: a name/value pair enclosed in braces).
-			    // Converting listing into a PHP associative array results in an
-			    // actual object when it's encoded.
-			    $listing = array( "filenames" => $listing);
-			    echo json_encode( $listing, JSON_PRETTY_PRINT);
-			} else {
-				// couldn't open the directory
-				header( 'HTTP/1.1 500 Internal Server Error');
-        		echo "Cannot open directory $dir.";
-        		return;
-			}
-		} else {
-			header( 'HTTP/1.1 400 Bad Request');
-        	echo "Unrecognized action variable.";
+       
+	// Check the 'action' var: should be either 'download', 'query' or 'upload'
+	// Handle file download requests
+	if ($action == "download") {
+		$fullPathName = find_directory( $pdo, $transId) . DIRECTORY_SEPARATOR . $file;	
+		if ( ! file_exists( $fullPathName)) {
+        	// file not found
+        	header( 'HTTP/1.1 404 Not Found');
+        	echo "The specified file - $file - could not be found.";
         	return;
+    	}
+
+	    if (filesize( $fullPathName) === false) {
+	        // File exists, but we can't read it.  Probably a permissions or
+	        // selinux issue.
+	        header( 'HTTP/1.1 500 Internal Server Error');
+	        echo "Cannot read $file.";
+	        return;
+	    }
+		
+		// Actually transfer the file
+        header( 'HTTP/1.1 200 OK');
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . $file);
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($fullPathName));
+        ob_clean();
+        flush();
+        readfile($fullPathName);
+		return;
+		
+	} elseif ($action == "query") {
+		// Get a directory listing and return it as a JSON object
+		$dir = find_directory( $pdo, $transId);
+		
+		$listing = array();
+		if ($handle = opendir($dir)) {
+		    // loop over the directory
+		    while (false !== ($entry = readdir($handle))) {
+		    	if ($entry != '.' && $entry != '..') {
+		    		$listing[] = $entry;
+		    		// appenda entry to the listing (but doesn't include
+		    		// . or .. directories)
+		    	}
+		    }
+
+		    closedir($handle);
+		    
+		    // If we just returned $listing now, we'd only get an array, not
+		    // a complete JSON object (ie: a name/value pair enclosed in braces).
+		    // Converting listing into a PHP associative array results in an
+		    // actual object when it's encoded.
+		    $listing = array( "filenames" => $listing);
+		    echo json_encode( $listing, JSON_PRETTY_PRINT);
+		} else {
+			// couldn't open the directory
+			header( 'HTTP/1.1 500 Internal Server Error');
+    		echo "Cannot open directory $dir.";
+    		return;
 		}
-	} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	
-		// TODO: Get the transaction ID from WHERE?!?!
+	} elseif ($action == "upload") {
 		$dir = find_directory( $pdo, $transId);
 		
 		// Check the $_FILES supergloblal
@@ -189,12 +214,16 @@
 					 echo "move_uploaded_file() failed:  ". $theFile['tmp_name'] . " ==> $pathname";
         		}
     		}
+    		else {
+    			header( 'HTTP/1.1 500 Internal Server Error');
+				echo "File upload failed.  Error #". $theFile['error'] . "\n";
+				echo "(See the PHP docs for the _FILES superglobal.)\n";
+				echo "Relevent info about the file:\n";
+				print_r( $theFile);
+    		}
 		}
-		
-	} else {
-        $msg = "Unrecognized request method: " . $_SERVER['REQUEST_METHOD'];
-        throw new MwsErrorCodeException( $msg);
-    }
+	}
+	
 	    
     } catch (DbException $e) {
         header( 'HTTP/1.1 500 Internal Server Error');
